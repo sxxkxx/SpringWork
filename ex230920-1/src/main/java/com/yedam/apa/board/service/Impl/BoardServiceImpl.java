92ae0d33a93a2942ddee883b39a00bf04d27196b
sxxkxx <seuld1521@gmail.com>
2023-09-20 17:50:33 +0900

    230920
    serviceimpl 만들기

---
+++ ex230920-1/src/main/java/com/yedam/apa/board/service/Impl/BoardServiceImpl.java
@@ -0,0 +1,40 @@
+package com.yedam.apa.board.service.Impl;
+
+import java.util.List;
+
+import com.yedam.apa.board.service.BoardService;
+import com.yedam.apa.board.service.BoardVO;
+
+public class BoardServiceImpl implements BoardService {
+
+	@Override
+	public List<BoardVO> getBoardList() {
+
+		return null;
+	}
+
+	@Override
+	public BoardVO getBoardInfo(BoardVO boardVO) {
+
+		return null;
+	}
+
+	@Override
+	public int insertBoardInfo(BoardVO boardVO) {
+
+		return 0;
+	}
+
+	@Override
+	public int updateBoardInfo(BoardVO boardVO) {
+
+		return 0;
+	}
+
+	@Override
+	public int deleteBoardInfo(int boardNo) {
+
+		return 0;
+	}
+
+}
